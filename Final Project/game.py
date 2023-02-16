117c30f0e17d3350133c237dbf44faaae18e16f5
Kristine <chankristineagcay@gmmail.com>
2023-02-16 18:27:50 +0800

    Implemented Main Menu buttons and Options

--- Final Project/game.py
+++ Final Project/game.py
@@ -98,3 +98,90 @@ def button2(t, xm, ym, x, y, wid, hei, int, after, fast):
     else:
         pygame.draw.rect(game_layout_display, int, [x, y, wid, hei])
     message_display_screen(t, (x + wid + x) / 2, (y + hei + y) / 2, fast)
+    
+
+# Quitting:
+def Quit():
+    pygame.quit()
+    quit()
+
+
+# Buttons:
+def button(t, xm, ym, x, y, wid, hei, int, after, fast, best):
+    if x + wid > xm > x and y + hei > ym > y:
+        pygame.draw.rect(game_layout_display, after, [x - 2.5, y - 2.5, wid + 5, hei + 5])
+        if pygame.mouse.get_pressed() == (1, 0, 0):
+            if best == 1:
+                choice()
+            elif best == 5:
+                return 5
+            elif best == 0:
+                Quit()
+            elif best == "s" or best == 2 or best == 3 or best == 4:
+                return best
+            elif best == 7:
+                choice()
+            else:
+                return True
+
+    else:
+        pygame.draw.rect(game_layout_display, int, [x, y, wid, hei])
+    message_display_screen(t, (x + wid + x) / 2, (y + hei + y) / 2, fast)
+    
+    
+# Main Menu
+def main_menu():
+    pygame.mixer.music.play(-1)
+
+    m = True
+    while m:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    Quit()
+
+        # mouse position
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        game_layout_display.blit(menubg, (0, 0))
+        button("Play", mouse[0], mouse[1], (width_screen / 2 - 100), (height_screen / 4), 200, 100, green_color,
+               blue_green_color, 60, 1)
+
+        button("Quit", mouse[0], mouse[1], (width_screen / 2 - 100), (height_screen / 5) + 200, 200, 100, red_color,
+               blue_red_color, 60, 0)
+        
+def creditation():
+    while True:
+        game_layout_display.blit(credits, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    Quit()
+        # mouse position
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if button("Back", mouse[0], mouse[1], width_screen / 2 - 100, 700, 200, 50, green_color, blue_red_color, 25, 20):
+            main_menu()
+
+        pygame.display.update()
+        mouse = pygame.mouse.get_pos()
+        if button2("Mute", mouse[0], mouse[1], 1166, 610, 200, 50, purple_color, blue_purple_color, 25):
+            pygame.mixer.music.pause()
+        if button2("Credits", mouse[0], mouse[1], 1166, 660, 200, 50, purple_color, blue_purple_color, 25):
+            creditation()
+
+        pygame.display.update()
+
+# Main Menu Options
+def choice():
+    f = True
+    while f == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    Quit()
